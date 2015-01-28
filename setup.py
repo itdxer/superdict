a37b2a53ff85e83cfe4f59722ccf77e841104b44
Yuri Shevchuk <yuri.shevchuk@mediasapiens.co>
2015-01-28 02:21:59 +0200

    Update version and .gitignore

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='SuperDict',
-    version='1.0',
+    version='1.0.0',
     description='SuperDict - it\'s like dict, but better',
     author='Yura Shevchuk',
     author_email='mail@itdxer.com',
